ff0053419db9a6f0aad1a75c97bbd998b7b8723c
Denise Moscon <denise.moscon@happybrain.it>
2019-11-20 16:10:54 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Goo from './Components/Goo'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Goo />
+    </div>
+  );
+}
+
+export default App;
