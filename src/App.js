25fafa16935ae761d0cb9c1da50e90000a359e8a
Denise Moscon <denise.moscon@happybrain.it>
2019-11-22 09:48:11 +0100

    add main nav

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import Goo from './Components/Goo'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Menu from './Components/Menu'
+import {Container} from "react-bootstrap";
 
 function App() {
   return (
+    <>
+    <Goo />
     <div className="App">
-      <Goo />
+      <Container className='position-relative' style={{zIndex: 100}}>
+      <Menu />
+      </Container>
     </div>
+      </>
   );
 }
 
---
+++ src/Components/Menu.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Nav} from "react-bootstrap";
+
+const Menu = () => {
+  return(
+    <Nav
+      activeKey="/home"
+      className='pt-5 justify-content-end'
+      //onSelect={selectedKey => alert(`selected ${selectedKey}`)}
+    >
+      <Nav.Item>
+        <Nav.Link href="/home">About Us</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">What we do</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">Team</Nav.Link>
+      </Nav.Item>
+    </Nav>
+  )
+}
+
+export default Menu
