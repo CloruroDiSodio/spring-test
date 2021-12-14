bf96bbb36e28764f94064dcfacd11d4eeed415d7
Denise Moscon <denise.moscon@vidiemme.it>
2021-12-14 23:48:01 +0100

    Feat(update): appetta

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React, {useEffect, useState} from 'react';
-import Goo from './components/atoms/Goo'
+import React from 'react';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Menu from './components/atoms/Menu'
 import {Container} from "react-bootstrap";
-import Circle from "./components/atoms/Circle";
-import {useLocation, HashRouter} from 'react-router-dom'
-import {useTransition, animated} from 'react-spring'
 import {FiRotateCcw} from 'react-icons/fi'
 import Navigation from "./navigation/navigation";
 
 function App() {
-
-  const [width, setWidth] = useState(window.innerWidth)
-
-  useEffect(() => {
-    window.addEventListener('resize', () => {
-      setWidth(window.innerWidth)
-    })
-  },[])
 
   return (
       <div className="App">
@@ -27,7 +14,6 @@ function App() {
             <p className='text-white text-center pt-5' style={{fontSize: 30}}>Please, rotate your device :)</p>
           </div>
           <Container className='position-relative app-container' style={{height: '100%'}} fluid>
-            {(width > 1024 )? <Goo /> : null}
 
             <Navigation />
           </Container>
--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -1,3 +1,4 @@
+import React, {useEffect, useState} from "react";
 import {
     BrowserRouter,
     Routes,
@@ -6,12 +7,13 @@ import {
 import Home from "../components/pages/Home";
 import Resume from "../components/pages/Resume";
 import Contacts from "../components/pages/Contacts";
-import React, {useEffect, useState} from "react";
+import Appetta from "../components/pages/Appetta/Appetta";
 import Menu from "../components/atoms/Menu";
 import Circle from "../components/atoms/Circle";
 import {useLocation} from 'react-router-dom'
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import './transitions.css'
+import Goo from "../components/atoms/Goo";
 
 
 const AnimatedSwitch = () => {
@@ -23,6 +25,7 @@ const AnimatedSwitch = () => {
                     <Route path="/" exact element={<Home />}/>
                     <Route path="/resume" exact element={<Resume />}/>
                     <Route path="/contacts" exact element={<Contacts />}/>
+                    <Route path="/appetta" exact element={<Appetta />}/>
                     <Route component={Home} />
                 </Routes>
             </CSSTransition>
@@ -30,8 +33,8 @@ const AnimatedSwitch = () => {
     );
 };
 
-
-const Navigation = props => {
+const Layout = () => {
+    const location = useLocation();
     const [width, setWidth] = useState(window.innerWidth)
 
     useEffect(() => {
@@ -39,13 +42,29 @@ const Navigation = props => {
             setWidth(window.innerWidth)
         })
     },[])
+    return (
+        <>
+            {location.pathname === '/appetta' ? <></> : (
+                <>
+                    {(width > 1024 )? <Goo /> : null}
+                    <Menu />
+                    <Circle className='circle-1'/>
+                    <Circle className='circle-2'/>
+                    {(width > 1024 )? null : <Circle className='circle-3'/>}
+                </>
+            )}
+
+        </>
+    );
+}
+
+
+const Navigation = props => {
+
 
     return (
         <BrowserRouter>
-            <Menu />
-            <Circle className='circle-1'/>
-            <Circle className='circle-2'/>
-            {(width > 1024 )? null : <Circle className='circle-3'/>}
+            <Layout />
             <AnimatedSwitch />
         </BrowserRouter>
     );
