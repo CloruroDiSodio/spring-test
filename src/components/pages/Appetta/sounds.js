a90ac17469aac325c87e952561c8aa2608673eac
Denise Moscon <denise.moscon@vidiemme.it>
2021-12-15 20:56:01 +0100

    Feat(appetta): words

--- src/components/pages/Appetta/sounds.js
+++ src/components/pages/Appetta/sounds.js
@@ -6,11 +6,13 @@ export const sounds = [
     {title: "Libretto", url: '/libretto.mp3'},
     {title: "Stufetta", url: '/stufetta.mp3'},
     {title: "Caffettino", url: '/caffettino.mp3'},
+    {title: "Pacchetto", url: '/pacchetto.mp3'},
 ]
 
 export const specials = [
     {title: "Ma dove vi porto", url: '/dove_vi_porto.mp3'},
     {title: "Poi vediamo", url: '/poi_vediamo.mp3'},
-    {title: "Tanto decide il fronted", url: '/decide-fe.mp3'},
+    {title: "Tanto decide il frontend", url: '/decide-fe.mp3'},
     {title: "Sono davanti ad un muro", url: '/muro.mp3'},
+    {title: "Che disagio", url: '/disagio.mp3'},
 ]
